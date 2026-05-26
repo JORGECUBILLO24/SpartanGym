17dbdfd1f672e01ed9f570cbcafa2f6012c56128
THEGABOALE <garciaga@uamv.edu.ni>
2026-05-26 13:40:10 -0600

    chore: inicializar el api rest de SpartanGym

---
+++ SpartanGymAPI/src/main/java/ni/edu/uam/SpartanGymAPI/SpartanGymApiApplication.java
@@ -0,0 +1,13 @@
+package ni.edu.uam.SpartanGymAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartanGymApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpartanGymApiApplication.class, args);
+	}
+
+}
