b77fcac0d832fc94c0fd875407b89213aa76959e
THEGABOALE <garciaga@uamv.edu.ni>
2026-05-28 11:54:06 -0600

    feat: crear servicio de autenticacion, controladores y repositorios para registro y login

--- SpartanGymAPI/src/main/java/ni/edu/uam/SpartanGymAPI/models/Socio.java
+++ SpartanGymAPI/src/main/java/ni/edu/uam/SpartanGymAPI/models/Socio.java
@@ -2,24 +2,24 @@ package ni.edu.uam.SpartanGymAPI.models;
 
 import jakarta.persistence.*;
 import lombok.Data;
-
 import java.util.UUID;
 
 @Data
 @Entity
 @Table(name = "socios")
 public class Socio {
+
     @Id
     @Column(name = "usuario_id")
     private UUID usuarioId;
 
     @OneToOne
-    @MapsId
+    @MapsId // Le dice a Spring que comparta la misma llave primaria (UUID) con la tabla Usuario
     @JoinColumn(name = "usuario_id")
     private Usuario usuario;
 
     @Column(nullable = false, length = 100)
-    private String nombre;
+    private String nombres;
 
     @Column(nullable = false, length = 100)
     private String apellidos;
@@ -28,5 +28,5 @@ public class Socio {
     private String telefono;
 
     @Column(name = "estado_acceso", length = 20)
-    private String estadoAccesp = "Inativo";
+    private String estadoAcceso = "Activo";
 }
