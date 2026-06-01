d337f14aeef8ce6f08b2c6fff662e2cd440400cc
THEGABOALE <garciaga@uamv.edu.ni>
2026-05-31 22:43:28 -0600

    fix: corregir tipo de dato de idEntrenador a UUID en RutinaRequest

--- SpartanGymAPI/src/main/java/ni/edu/uam/SpartanGymAPI/dto/RutinaRequest.java
+++ SpartanGymAPI/src/main/java/ni/edu/uam/SpartanGymAPI/dto/RutinaRequest.java
@@ -2,12 +2,13 @@ package ni.edu.uam.SpartanGymAPI.dto;
 
 import lombok.Data;
 import java.util.List;
+import java.util.UUID;
 
 @Data
 public class RutinaRequest {
     private String nombre;
     private String descripcion;
     private String nivel;
-    private Long idEntrenador;
+    private UUID idEntrenador; // <-- Cambiado de Long a UUID
     private List<RutinaDetalleRequest> detalles;
 }
