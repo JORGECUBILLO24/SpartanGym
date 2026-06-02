786bf191be70df46b0748a8d1af4bec112c6d07b
THEGABOALE <garciaga@uamv.edu.ni>
2026-06-02 15:54:12 -0600

    feat: finalizar modulo de pagos y renovacion automatica de membresias con activacion de socio

--- SpartanGymAPI/src/main/java/ni/edu/uam/SpartanGymAPI/repositories/TipoMembresiaRepository.java
+++ SpartanGymAPI/src/main/java/ni/edu/uam/SpartanGymAPI/repositories/TipoMembresiaRepository.java
@@ -4,9 +4,6 @@ import ni.edu.uam.SpartanGymAPI.models.TipoMembresia;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface TipoMembresiaRepository extends JpaRepository<TipoMembresia, Integer> {
-    Optional<TipoMembresia> findByNombre(String nombre);
 }
