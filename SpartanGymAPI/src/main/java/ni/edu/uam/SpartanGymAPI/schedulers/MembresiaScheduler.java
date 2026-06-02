e6a6bb5ab8e8f0b20d930cc1f96d1a7504e278e3
THEGABOALE <garciaga@uamv.edu.ni>
2026-06-02 16:34:46 -0600

    feat: implementar cron job para invalidar membresias vencidas y bloquear acceso

---
+++ SpartanGymAPI/src/main/java/ni/edu/uam/SpartanGymAPI/schedulers/MembresiaScheduler.java
@@ -0,0 +1,54 @@
+package ni.edu.uam.SpartanGymAPI.schedulers;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import ni.edu.uam.SpartanGymAPI.models.MembresiaSocio;
+import ni.edu.uam.SpartanGymAPI.models.Socio;
+import ni.edu.uam.SpartanGymAPI.repositories.MembresiaSocioRepository;
+import ni.edu.uam.SpartanGymAPI.repositories.SocioRepository;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Slf4j // Para poder imprimir mensajes bonitos en la consola
+@Component
+@RequiredArgsConstructor
+public class MembresiaScheduler {
+
+    private final MembresiaSocioRepository membresiaRepository;
+    private final SocioRepository socioRepository;
+
+    // "0 0 0 * * *" significa: Todos los días a las 00:00 (Medianoche)
+    // Para hacer pruebas rápidas podrías usar @Scheduled(fixedRate = 60000) que corre cada 1 minuto
+    @Scheduled(cron = "0 0 0 * * *")
+    @Transactional
+    public void verificarMembresiasVencidas() {
+        log.info("Iniciando revisión automática de membresías vencidas...");
+
+        // Buscamos las que están "Activas" pero su fecha de vencimiento es ANTES de hoy
+        List<MembresiaSocio> vencidas = membresiaRepository.findByEstadoAndFechaVencimientoBefore("Activa", LocalDate.now());
+
+        int contador = 0;
+        for (MembresiaSocio membresia : vencidas) {
+            // 1. Vencemos la membresía
+            membresia.setEstado("Vencida");
+            membresiaRepository.save(membresia);
+
+            // 2. Le bloqueamos el torniquete al socio
+            Socio socio = membresia.getSocio();
+            socio.setEstadoAcceso("Inactivo");
+            socioRepository.save(socio);
+
+            contador++;
+            log.info("Membresía vencida para el socio: {} {}", socio.getNombres(), socio.getApellidos());
+
+            // ¡AQUÍ ES DONDE LLAMAREMOS AL SISTEMA DE NOTIFICACIONES EN EL SIGUIENTE PASO!
+            // notificacionService.enviarCorreoVencimiento(socio.getUsuario().getEmail());
+        }
+
+        log.info("Revisión terminada. Membresías actualizadas: {}", contador);
+    }
+}
