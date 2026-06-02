7efbbeb61261a7e02c0eb7bd5e1a2a7e49f8f042
THEGABOALE <garciaga@uamv.edu.ni>
2026-06-02 16:34:46 -0600

    feat: implementar cron job para invalidar membresias vencidas y bloquear acceso

--- SpartanGymAPI/src/main/java/ni/edu/uam/SpartanGymAPI/repositories/MembresiaSocioRepository.java
+++ SpartanGymAPI/src/main/java/ni/edu/uam/SpartanGymAPI/repositories/MembresiaSocioRepository.java
@@ -4,11 +4,17 @@ import ni.edu.uam.SpartanGymAPI.models.MembresiaSocio;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface MembresiaSocioRepository extends JpaRepository<MembresiaSocio, UUID> {
-    // Usamos SocioUsuarioId porque es la llave real del Socio
+
+    // Usamos SocioUsuarioId porque es la llave real del Socio (usado en BFF y Pagos)
     Optional<MembresiaSocio> findBySocioUsuarioIdAndEstado(UUID socioId, String estado);
+
+    // NUEVO: Para el Cron Job (Busca las membresías activas cuya fecha ya expiró)
+    List<MembresiaSocio> findByEstadoAndFechaVencimientoBefore(String estado, LocalDate fecha);
 }
